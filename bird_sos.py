c2dfbaf6fb1f28a23700fbcea1f5e309fd408bef
XtinctBird <challenges4u4ctfpurposes@outlook.com>
2024-11-17 17:17:01 +0000

    A poor bird is asking for help!!

---
+++ bird_sos.py
@@ -0,0 +1,10 @@
+"""
+HELP!  
+
+The flag was here, but it flew away.  
+Last seen heading toward a suspicious link…  
+You might want to check that out. Or not.  
+
+Signed,  
+A very distressed bird.
+"""
